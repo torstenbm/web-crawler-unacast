88b4cf5bc0bd0cd5e0279314f0ae1e6874fd63a6
Torsten Moss <tbm94@live.no>
2020-01-13 13:55:42 +0100

    Functionalize code a bit

--- index.js
+++ index.js
@@ -4,36 +4,49 @@ const port = 3000
 const axios = require('axios');
 const cheerio = require('cheerio');
 const uuidv1 = require('uuid/v1');
+const urlRegex = require('url-regex');
 
 app.use(express.json());
-
-app.get('/', (req, res) => res.send('Hello World!'))
 
 // In memory DB for scalability and security
 const DATABASE = {};
 
-app.get('/contents/:webPageId', (req, res) => {
-    let resultObj = DATABASE[req.params.webPageId];
-    res.send(resultObj);
-})
+const writeCrawlResultToDB = response => {
+    const ID = uuidv1();
+    const $ = cheerio.load(response.data);
+    DATABASE[ID] = {
+        "title": $('title').text(),
+        "date": new Date().toString(),
+        "url": response.config.url,
+        "contents": response.data
+    }
+    return ID
+}
 
 app.post('/schedule', (req, res) => {
     const URL = req.body.URL;
-    axios.get(URL)
-        .then(response => {
-            const ID = uuidv1();
-            const $ = cheerio.load(response.data);
+    
+    if (!urlRegex().test(URL)){
+        res.send({"error": "invalid URL"});
+    }
+    else {
+        axios.get(URL)
+            .then(writeCrawlResultToDB)
+            .then(ID => res.send({"id": ID}))
+            .catch(console.log);
+    }
+})
 
-            DATABASE[ID] = {
-                "title": $('title').text(),
-                "date": new Date().toString(),
-                "url": URL,
-                "contents": response.data
-            }
+app.get('/pages/:webPageId', (req, res) => {
+    const ID = req.params.webPageId
 
-            res.send({"id": ID});
-        })
-        .catch(console.log);
-}) 
+    if (!(ID in DATABASE)) {
+        res.send({"error": "page not found in database"});
+    }
+    else {
+        const resultObj = DATABASE[req.params.webPageId];
+        res.send(resultObj);
+    }
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Crawler running on port ${port}!`))
